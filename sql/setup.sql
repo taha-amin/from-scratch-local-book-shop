a5b559e16df6565103da88070d90d23d34840120
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-06-14 10:05:06 -0700

    in setup.sql defined the SQL table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,16 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR(280) NOT NULL,
+    released NOT NULL
+);
+
+INSERT INTO books (
+    title,
+    released
+)
+VALUES
+    ('Lord of the Things', 1922)
