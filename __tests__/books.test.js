d7db4e931e79eea2a5c0bd6d1cadf15465c79d69
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-06-14 09:21:52 -0700

    changed episodes to books on line 6

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -3,7 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('episodes routes', () => {
+describe('books routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
