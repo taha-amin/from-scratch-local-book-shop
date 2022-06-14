e7a958fc399f980b7a9255b511596fb2bf471557
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-06-14 14:24:31 -0700

    in books.test.js for first test refactored code to include code in setup.sql

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -10,7 +10,29 @@ describe('books routes', () => {
 
   it('should return a list of books', async () => {
     const res = await request(app).get('/books');
-    expect(res.body.length).toEqual(1);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual([
+      {
+        id: '1',
+        title: 'Lord of the Things',
+        released: 1922,
+        authors: [
+          { id: '1', name: 'Arthur McArthur', dob: 1959, pob: 'Orange County' },
+        ],
+      },
+      {
+        id: '2',
+        title: 'Of Mice and Men',
+        released: 1999,
+        authors: [{ id: '2', name: 'Test McTest', dob: 1960, pob: 'New York' }],
+      },
+      {
+        id: '3',
+        title: 'Harry Potter',
+        released: 2000,
+        authors: [{ id: '3', name: 'Another McTest', dob: 1890, pob: 'Miami' }],
+      },
+    ]);
   });
 
   it('should return book detail', async () => {
