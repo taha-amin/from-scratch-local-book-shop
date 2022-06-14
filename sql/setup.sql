d2e6c15a94c9c9c39d3558053240463c08558b7f
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-06-14 12:10:15 -0700

    in setup.sql created authors table with INSERT INTO and VALUES

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS authors;
 
 CREATE TABLE books (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -14,3 +15,18 @@ INSERT INTO books (
 )
 VALUES
     ('Lord of the Things', 1922);
+
+CREATE TABLE authors (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(280) NOT NULL,
+    dob INT NOT NULL,
+    pob VARCHAR(280) NOT NULL,
+)
+
+INSERT INTO authors (
+    name,
+    dob,
+    pob
+)
+VALUES
+    ('Arthur McArthur', 1959, 'Orange County');
